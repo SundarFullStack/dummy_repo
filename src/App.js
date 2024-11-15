9982393d890e262f35cdd2f0cf40249fa609a6c3
SundarFullStack <meenakshisunder183017@gmail.com>
2024-11-16 05:07:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ marginTop: "50vh" }}>It's my First Git Hosting</h1>
+    </div>
+  );
+}
+
+export default App;
